5383ff62f291a0c9aec6bdcdbff1e680c0c21785
Alperen Orhan <alperenorhan5552@gmail.com>
2022-04-22 09:42:21 +0300

    test_text, test_html, test_json routes working

--- src/app.js
+++ src/app.js
@@ -6,3 +6,27 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("Sunucu ayakta.")
 });
+
+app.get("/test_text", (req, res) => {
+    res.write("19360859025");
+    res.end();
+})
+app.get("/test_html", (req, res) => {
+    res.write("<body><h1>Alperen ORHAN</h1></body>")
+    res.end();
+})
+app.get("/test_json", (req, res) => {
+    res.json({
+        "ad": "Alperen",
+        "soyad": "Orhan"
+    })
+})
+app.get("/homepage", (req, res) => {
+    console.log("Sayfaya girildi.")
+})
+app.get("/test_geocode", (req, res) => {
+    console.log("Sayfaya girildi.")
+})
+app.get("/test_weather", (req, res) => {
+    console.log("Sayfaya girildi.")
+})
